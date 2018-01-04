e21186835e2e4f869fb629d6540e1a0625bc83ef
raagu <raghavendramr1993@gmail.com>
2018-01-04 21:27:03 +0530

    updated code

--- cal.js
+++ cal.js
@@ -1,25 +1,76 @@
 function keyPadClickHandler(event) {
     var pressedKeyText = getPressedKeyFromEvent(event);  
-    document.getElementById("display").innerHTML += pressedKeyText;
+     document.getElementById("display").innerText += pressedKeyText;
     
 };
 
 function getPressedKeyFromEvent(event) {
     return event.currentTarget.innerText;
 };
-
-function evalu() {
-    var cal1 = document.getElementById("display").innerHTML;
-    var calculate = eval(cal1);
-    document.getElementById("display").innerHTML = calculate;
+function add(LeftOperand, RightOperand){
+    var total = Number(LeftOperand) + Number(RightOperand);
+    document.getElementById("display").innerText = total;
 };
 
+function subtract(LeftOperand, RightOperand){
+    var total = Number(LeftOperand) - Number(RightOperand);
+    document.getElementById("display").innerText = total;
+};
+
+function multiply(LeftOperand, RightOperand){
+    var total = Number(LeftOperand) * Number(RightOperand);
+    document.getElementById("display").innerText = total;
+};
+
+function divid(LeftOperand, RightOperand){
+    var total = Number(LeftOperand) / Number(RightOperand);
+    document.getElementById("display").innerText = total;
+};
+
+function percent(LeftOperand, RightOperand){
+    var total = Number(LeftOperand) % Number(RightOperand);
+    document.getElementById("display").innerText = total;
+};
+
+function evaluateValues() {
+    var EnteredValues = document.getElementById("display").innerText;
+    if (EnteredValues.includes("+")){
+        var LeftOperand = EnteredValues.substring(0, EnteredValues.indexOf("+"));
+        var RightOperand = EnteredValues.substring(EnteredValues.indexOf("+")+1);
+       return Execute(LeftOperand, RightOperand, add);
+    }
+    else if (EnteredValues.includes("-")){
+        var LeftOperand = EnteredValues.substring(0, EnteredValues.indexOf("-"));
+        var RightOperand = EnteredValues.substring(EnteredValues.indexOf("-")+1);
+        return Execute(LeftOperand, RightOperand, subtract);
+    }
+    else if (EnteredValues.includes("*")){
+        var LeftOperand = EnteredValues.substring(0, EnteredValues.indexOf("*"));
+        var RightOperand = EnteredValues.substring(EnteredValues.indexOf("*")+1);
+        return Execute(LeftOperand, RightOperand, multiply);
+    }
+    else if (EnteredValues.includes("/")){
+        var LeftOperand = EnteredValues.substring(0, EnteredValues.indexOf("/"));
+        var RightOperand = EnteredValues.substring(EnteredValues.indexOf("/")+1);
+        return Execute(LeftOperand, RightOperand, divid);
+    }
+    else if (EnteredValues.includes("%")){
+        var LeftOperand = EnteredValues.substring(0, EnteredValues.indexOf("%"));
+        var RightOperand = EnteredValues.substring(EnteredValues.indexOf("%")+1);
+        return Execute(LeftOperand, RightOperand, percent);
+    }
+};
+
+function Execute(LeftOperand, RightOperand, callback){
+    return callback(LeftOperand, RightOperand)
+}
+
 function clearScreen(){
-    document.getElementById("display").innerHTML = "";
+    document.getElementById("display").innerText = "";
 }
 
 function backSpace() {
-    var bspace = document.getElementById("display").innerHTML;
-    var res = bspace.substring(0, bspace.length -1);
-    document.getElementById("display").innerHTML = res;
+    var backSpace = document.getElementById("display").innerText;
+    var clear = backSpace.substring(0, backSpace.length -1);
+    document.getElementById("display").innerText = clear;
 }
